f82189c10e9b198b188ffdf8b0b740a0defc4250
jacques-twizeyimana <sandbergjacques500@gmail.com>
2024-03-05 18:12:09 +0200

    Remove unnecessary code and add missing newline at end of file

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -154,8 +154,3 @@ class Rectangle:
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
         return rect_1 if rect_1.area() >= rect_2.area() else rect_2
-
-
-mysquare = Rectangle.square(5)
-print(mysquare.width)
-# print(mysquare)
