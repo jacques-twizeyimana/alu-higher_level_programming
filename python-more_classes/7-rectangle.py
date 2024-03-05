e5bc6effc586c65bcc5368edd265cc52f5388826
jacques-twizeyimana <sandbergjacques500@gmail.com>
2024-03-05 17:57:28 +0200

    fix: cast as string before printing a class

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -101,7 +101,7 @@ class Rectangle:
         """
         if self.width == 0 or self.height == 0:
             return ""
-        return "\n".join([self.print_symbol * self.width] * self.height)
+        return "\n".join([str(self.print_symbol) * self.width] * self.height)
 
     def __repr__(self):
         """
